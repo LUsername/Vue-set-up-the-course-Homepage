375332f4738248e984a9afde9eaa5fd8e6e6a898
LUsername <492254446@qq.com>
2019-11-24 15:51:50 +0800

    完成detail页面视频播放区样式

--- src/page/detail.vue
+++ src/page/detail.vue
@@ -1,5 +1,33 @@
 <template>
-    <div class="detail-page">
-        detail
-    </div>
+	<div class="detail-page">
+		<div class="video">
+			<video src="http://v3.mukewang.com/shizhan/59f8498ae420e5be578b459b/H.mp4" controls="controls"></video>
+			<router-link to="/">
+				<div class="video-back"><</div>
+			</router-link>
+		</div>
+	</div>
 </template>
+<style scoped>
+	.video{
+		position: relative;
+		height: 3.82rem;
+	}
+	.video video{
+		width: 100%;
+		height: 100%;
+	}
+	.video-back{
+		position: absolute;
+		top: 21px;
+		left: 16px;
+		text-align: center;
+		color: #fff;
+		font-size: 18px;
+		line-height: 32px;
+		border-radius: 3px;
+		width: 32px;
+		height: 32px;
+		background: rgba(0,0,0,.5);
+	}
+</style>
