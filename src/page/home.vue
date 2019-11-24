268a145ca1490a675718981093da7f36b3b6366b
LUsername <492254446@qq.com>
2019-11-24 14:48:49 +0800

    实现课程列表组件

--- src/page/home.vue
+++ src/page/home.vue
@@ -2,25 +2,34 @@
     <div class="home-page">
         <v-header></v-header>
         <ul class="nav">
-            <li v-for="item in arrList" :key="item"><img :src="item.imgUrl"><span>{{item.name}}</span></li>
+            <li v-for="item in navArrList" :key="item"><img :src="item.imgUrl"><span>{{item.name}}</span></li>
         </ul>
+        <v-course :arrList="array"></v-course>
     </div>
 </template>
 <script>
-import Header from '@/components/header'
-import WebImg from '@/assets/web.png'
-import WebService from '@/assets/webservice.png'
-import MoveImg from '@/assets/move.png'
-import DataImg from '@/assets/data.png'
-import DownloadImg from '@/assets/download.png'
-export default {
+    import Header from '@/components/header'
+    import WebImg from '@/assets/web.png'
+    import WebService from '@/assets/webservice.png'
+    import MoveImg from '@/assets/move.png'
+    import DataImg from '@/assets/data.png'
+    import DownloadImg from '@/assets/download.png'
+    import Course from '@/components/course'
+   
+   	import course1 from "@/assets/course1.jpg";
+	import course2 from "@/assets/course2.jpg";
+	import course3 from "@/assets/course3.jpg";
+	import course4 from "@/assets/course4.jpg";
+	import course5 from "@/assets/course5.jpg";
+    export default {
     data(){
         return{
-            arrList:[]
+            navArrList:[],
+            array:[]
         }
     },
     mounted(){
-        this.arrList = [{
+        this.navArrList = [{
             imgUrl:WebImg,
             name:'前端开发'
         },{
@@ -35,10 +44,48 @@ export default {
         },{
             imgUrl:DownloadImg,
             name:'云计算'
-        }]
+        }];
+        this.array = [
+				{
+					imgUrl: course1,
+					title: '实例妙解Sed和Awk的秘密',
+					msg: '中级 · 330人在学'
+				},
+				{
+					imgUrl: course2,
+					title: '实例妙解Sed和Awk的秘密',
+					msg: '中级 · 330人在学'
+				},
+				{
+					imgUrl: course3,
+					title: '实例妙解Sed和Awk的秘密',
+					msg: '中级 · 330人在学'
+				},
+				{
+					imgUrl: course4,
+					title: '实例妙解Sed和Awk的秘密',
+					msg: '中级 · 330人在学'
+				},
+				{
+					imgUrl: course5,
+					title: '实例妙解Sed和Awk的秘密',
+					msg: '中级 · 330人在学'
+				},
+				{
+					imgUrl: course1,
+					title: '实例妙解Sed和Awk的秘密',
+					msg: '中级 · 330人在学'
+				},
+				{
+					imgUrl: course1,
+					title: '实例妙解Sed和Awk的秘密',
+					msg: '中级 · 330人在学'
+				}
+			];
     },
     components:{
-        'v-header':Header
+        'v-header':Header,
+        'v-course':Course
     }
 }
 </script>
